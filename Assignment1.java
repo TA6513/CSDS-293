5e0b1f67eba24da562988148fb5a3b0a8ca89115
squidproquo <tyler08sd@gmail.com>
2023-09-06 05:12:41 -0400

    comments

--- Assignment1.java
+++ Assignment1.java
@@ -5,21 +5,21 @@ public class Assignment1 {
     public static <T extends Comparable<? super T>> List<T> longestHigherSuffix(
             List<T> a, List<T> b, Comparator<T> cmp) {
 
-        int minLength = Math.min(a.size(), b.size());
-
-        List<T> reversedA = new ArrayList<>(a);
+        int minLength = Math.min(a.size(), b.size()); //get the length of the shortest list
+        List<T> reversedA = new ArrayList<>(a); //create a reversed version of list a for easier traversal
         Collections.reverse(reversedA);
 
-        List<T> reversedB = new ArrayList<>(b);
+        List<T> reversedB = new ArrayList<>(b); //create a reversed version of list b for easier traversal
         Collections.reverse(reversedB);
 
-        List<T> result = IntStream.range(0, minLength)
-                .takeWhile(i -> cmp.compare(reversedA.get(i), reversedB.get(i)) >= 0)
-                .mapToObj(i -> reversedA.get(i))
-                .collect(Collectors.toList());
+        List<T> result = IntStream.range(0, minLength) //create stream of indices from 0 to minLength
+                .takeWhile(i -> cmp.compare(reversedA.get(i), reversedB.get(i)) >= 0) //filter stream to only include 
+                                                                                      //indices where a >= b
+                .mapToObj(i -> reversedA.get(i))                                      //converts elements at indices to stream
+                .collect(Collectors.toList());                                        //combine elements of suffix in a list
 
-        Collections.reverse(result);
-        return result;
+        Collections.reverse(result); //reverse the list to undo the original reversal
+        return result; //return the list
     }
 
     public static void main(String[] args) {
