d92202611600c6877b589710abbbfea1c043ba65
squidproquo <tyler08sd@gmail.com>
2023-09-27 16:05:06 -0400

    final assignment 4 commit

--- Assignment2/ring/Indexes.java
+++ Assignment2/ring/Indexes.java
@@ -1,29 +1,37 @@
 package ring;
 import java.util.stream.Stream;
 
+// A record representing the indices of a matrix.
 public record Indexes(int row, int column) implements Comparable<Indexes> {
     
+    // Returns the value at these indices in a given 2D matrix.
     public <S> S value(S[][] matrix) {
         return matrix[row][column];
     }
 
+    // Checks if the indices represent a position on the matrix's diagonal.
     public boolean areDiagonal() {
         return row == column;
     }
 
+    // Generates a stream of indices from 'from' to 'to' inclusive.
     public static Stream<Indexes> stream(Indexes from, Indexes to) {
-        return Stream.iterate(from, i -> i.row < to.row || (i.row == to.row && i.column < to.column),
-                i -> i.column == to.column ? new Indexes(i.row + 1, from.column) : new Indexes(i.row, i.column + 1));
+        return Stream.iterate(from,
+            i -> i.row < to.row || (i.row == to.row && i.column < to.column),
+            i -> i.column == to.column ? new Indexes(i.row + 1, from.column) : new Indexes(i.row, i.column + 1));
     }
 
+    // Generates a stream of indices from (0, 0) to 'size' inclusive.
     public static Stream<Indexes> stream(Indexes size) {
         return stream(new Indexes(0, 0), size);
     }
 
+    // Generates a stream of indices from (0, 0) to (rows - 1, columns - 1) inclusive.
     public static Stream<Indexes> stream(int rows, int columns) {
         return stream(new Indexes(rows - 1, columns - 1));
     }
 
+    // Compares two sets of indices for ordering purposes.
     @Override
     public int compareTo(Indexes other) {
         int rowComparison = Integer.compare(row, other.row);
--- Assignment2/ring/MatrixMap.java
+++ Assignment2/ring/MatrixMap.java
@@ -1,7 +1,9 @@
 package ring;
+
 import java.util.*;
 import java.util.function.Function;
 
+// Represents a generic matrix. Provides methods for creating, accessing, and manipulating matrices of arbitrary types.
 public final class MatrixMap<T> {
     private final Map<Indexes, T> matrix;
     private final Indexes size;
@@ -11,11 +13,9 @@ public final class MatrixMap<T> {
         this.size = size;
     }
 
-    public static <S> MatrixMap<S> instance(int rows, int columns, Function<Indexes, S> valueMapper) {
-        if (rows <= 0 || columns <= 0) {
-            throw new IllegalArgumentException("Number of rows and columns must be greater than zero.");
-        }
-
+    // Creates a matrix using and initializes its elements based on the provided
+    // valueMapper function.
+    private static <S> Map<Indexes, S> createMatrix(int rows, int columns, Function<Indexes, S> valueMapper) {
         Map<Indexes, S> matrix = new HashMap<>();
         Indexes size = new Indexes(rows - 1, columns - 1);
 
@@ -26,53 +26,54 @@ public final class MatrixMap<T> {
             }
         }
 
-        return new MatrixMap<>(matrix, size);
+        return matrix;
     }
 
+    // Creates a matrix with specified dimensions and initializes values using a
+    // valueMapper function.
+    public static <S> MatrixMap<S> instance(int rows, int columns, Function<Indexes, S> valueMapper) {
+        if (rows <= 0 || columns <= 0) {
+            throw new IllegalArgumentException("Number of rows and columns must be greater than zero.");
+        }
+
+        return new MatrixMap<>(createMatrix(rows, columns, valueMapper), new Indexes(rows - 1, columns - 1));
+    }
+
+    // Creates a matrix with specified dimensions and initializes values using a
+    // valueMapper function.
     public static <S> MatrixMap<S> instance(Indexes size, Function<Indexes, S> valueMapper) {
         return instance(size.row() + 1, size.column() + 1, valueMapper);
     }
 
+    // Creates a constant matrix of a given size with all elements initialized to a
+    // specific value.
     public static <S> MatrixMap<S> constant(int size, S value) {
         if (size <= 0) {
             throw new IllegalArgumentException("Size must be greater than zero.");
         }
 
-        Map<Indexes, S> matrix = new HashMap<>();
-        Indexes sizeIndex = new Indexes(size - 1, size - 1);
-
-        for (int row = 0; row <= sizeIndex.row(); row++) {
-            for (int col = 0; col <= sizeIndex.column(); col++) {
-                Indexes index = new Indexes(row, col);
-                matrix.put(index, value);
-            }
-        }
-
-        return new MatrixMap<>(matrix, sizeIndex);
+        return new MatrixMap<>(createMatrix(size, size, index -> value), new Indexes(size - 1, size - 1));
     }
 
+    // Creates an identity matrix of a given size with specified zero and identity
+    // values.
     public static <S> MatrixMap<S> identity(int size, S zero, S identity) {
         if (size <= 0) {
             throw new IllegalArgumentException("Size must be greater than zero.");
         }
 
-        Map<Indexes, S> matrix = new HashMap<>();
-        Indexes sizeIndex = new Indexes(size - 1, size - 1);
-
-        for (int row = 0; row <= sizeIndex.row(); row++) {
-            for (int col = 0; col <= sizeIndex.column(); col++) {
-                Indexes index = new Indexes(row, col);
-                S value = (index.areDiagonal()) ? identity : zero;
-                matrix.put(index, value);
-            }
-        }
-
-        return new MatrixMap<>(matrix, sizeIndex);
+        return new MatrixMap<>(createMatrix(size, size, index -> (index.areDiagonal()) ? identity : zero),
+                new Indexes(size - 1, size - 1));
     }
 
+    // Creates a matrix from a two-dimensional array of values.
     public static <S> MatrixMap<S> from(S[][] matrixArray) {
         int rows = matrixArray.length;
         int columns = (rows > 0) ? matrixArray[0].length : 0;
+
+        if (rows <= 0 || columns <= 0) {
+            throw new IllegalArgumentException("Number of rows and columns must be greater than zero.");
+        }
 
         Map<Indexes, S> matrix = new HashMap<>();
         Indexes size = new Indexes(rows - 1, columns - 1);
@@ -80,7 +81,7 @@ public final class MatrixMap<T> {
         for (int row = 0; row <= size.row(); row++) {
             for (int col = 0; col <= size.column(); col++) {
                 Indexes index = new Indexes(row, col);
-                if (row >= 0 && row < rows && col >= 0 && col < columns) {
+                if (row < rows && col < columns) {
                     matrix.put(index, matrixArray[row][col]);
                 }
             }
@@ -89,41 +90,44 @@ public final class MatrixMap<T> {
         return new MatrixMap<>(matrix, size);
     }
 
+    // Returns the size of the matrix.
     public Indexes size() {
         return size;
     }
 
+    // Returns the value at the specified indexes in the matrix.
     public T value(Indexes indexes) {
         return matrix.getOrDefault(indexes, null);
     }
 
+    // Returns the value at the specified row and column in the matrix.
     public T value(int row, int column) {
         return value(new Indexes(row, column));
     }
 
+    // Returns the value at the specified indexes in a given matrix.
     public static <S> S value(MatrixMap<S> matrix, Indexes indexes) {
         return matrix.value(indexes);
     }
 
+    // Generates a string representation of the matrix.
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        for (int row = 0; row <= size.row(); row++) {
-            for (int col = 0; col <= size.column(); col++) {
+        for (int row = 0, numRows = size.row(), numCols = size.column(); row <= numRows; row++) {
+            for (int col = 0; col <= numCols; col++) {
                 Indexes index = new Indexes(row, col);
-                T value = matrix.get(index);
+                T value = matrix.getOrDefault(index, null);
                 sb.append((value != null) ? value.toString() : "").append("\t");
-
-                if (col == size.column()) {
-                    sb.append("\n");
-                }
             }
+            sb.append("\n");
         }
 
         return sb.toString();
     }
 
+    // Custom exception class for invalid length with cause and length information.
     public static class InvalidLengthException extends IllegalArgumentException {
         public enum Cause {
             ROW, COLUMN
@@ -151,5 +155,4 @@ public final class MatrixMap<T> {
             return "Invalid length for " + cause.toString() + ": " + length;
         }
     }
-
 }
